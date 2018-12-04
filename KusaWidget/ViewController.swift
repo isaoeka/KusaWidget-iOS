a253623d083fe46d9991e85eff88e296d7e59e63
isaoeka <isaoeka@gmail.com>
2018-12-04 18:18:45 +0900

    :+1: Initial commit

---
+++ KusaWidget/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KusaWidget
+//
+//  Created by isaoeka on 2018/12/04.
+//  Copyright © 2018 isaoeka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
