03d6667bb47d0e218c8e06595f816228d222bfe0
isaoeka <isaoeka@gmail.com>
2018-12-05 17:09:45 +0900

    :sparkles: Add SquareCell

--- KusaTodayExtension/TodayViewController.swift
+++ KusaTodayExtension/TodayViewController.swift
@@ -17,7 +17,7 @@ class TodayViewController: UIViewController, NCWidgetProviding {
         super.viewDidLoad()
         // Do any additional setup after loading the view from its nib.
         
-        self.collectionView.backgroundColor = .clear
+        self.initView()
     }
         
     func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
@@ -30,4 +30,43 @@ class TodayViewController: UIViewController, NCWidgetProviding {
         completionHandler(NCUpdateResult.newData)
     }
     
+}
+
+extension TodayViewController {
+    private func initView() {
+        let nib = UINib(nibName: "SquareCell", bundle: nil)
+        self.collectionView.register(nib, forCellWithReuseIdentifier: "SquareCell")
+        self.collectionView.backgroundColor = .clear
+        self.collectionView.collectionViewLayout = UICollectionViewFlowLayout().apply {
+            $0.scrollDirection = .horizontal
+            $0.minimumInteritemSpacing = 1
+            $0.minimumLineSpacing = 1
+
+            let rect = self.collectionView.bounds
+            let width = rect.width / (364 / 7) - $0.minimumInteritemSpacing
+            let height = rect.height / 7 - $0.minimumLineSpacing
+            $0.itemSize = CGSize(width: width, height: height)
+        }
+
+        self.collectionView.delegate = self
+        self.collectionView.dataSource = self
+    }
+}
+
+// MARK: - UICollectionViewDataSource, UICollectionViewDelegate
+extension TodayViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 365
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "SquareCell", for: indexPath) as! SquareCell
+        return cell
+    }
+
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+
+    }
+
 }
---
+++ KusaTodayExtension/Views/SquareCell.swift
@@ -0,0 +1,19 @@
+//
+//  SquareCell.swift
+//  KusaTodayExtension
+//
+//  Created by isaoeka on 2018/12/05.
+//  Copyright © 2018 isaoeka. All rights reserved.
+//
+
+import UIKit
+
+class SquareCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        self.backgroundColor = .blue
+    }
+
+}
